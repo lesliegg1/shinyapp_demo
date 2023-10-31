a8210266b8484ca360f173133abd5e06e1bf1cdb
Leslie <lgermain@neptuneinc.org>
2023-10-30 20:39:51 -0600

    added intiial null and alternative plots

--- app.R
+++ app.R
@@ -1,12 +1,13 @@
 
 
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Statistical Analysis Widget"),
+    titlePanel("Power Analysis Widget"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -27,7 +28,7 @@ ui <- fluidPage(
                                        value = .8)),
           #significance level
           conditionalPanel(condition = "input.solveFor != 'Significance Level'",
-                           sliderInput(inputId = "significanceLevel",
+                           sliderInput(inputId = "alpha",
                                        label = "Significance Level",
                                        min = 0,
                                        max = 1,
@@ -55,13 +56,41 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           
+          fluidRow(
+            column(6,
+                   plotOutput("nulldistribution")
+            ),
+            column(6,
+                   plotOutput("altdistribution")
+            )
+          ) # end fluidRow
         )# end main
     )# end layout
 )# end ui
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
+
+  # critical value for the selected alpha
+  zcrit <- reactive({
+    qnorm(1-input$alpha/2, 0, 1/input$sampleSize)
+  }) # assuming two-tailed
+  
+  # plot the distribution of the mean under the null, assuming mu=0 and sigma2=1
+  output$nulldistribution <- renderPlot({
+    #generate a normal distribution plot
+    ggplot(data.frame(x = c(-1, 1)), aes(x = x)) +
+      stat_function(fun = dnorm, args=c(0, 1/input$sampleSize), n=1000) +
+      geom_vline(aes(xintercept=zcrit()))
+  })
+  
+  # plot the distribution of the mean under the alternative, assuming mu=0+effectsize and sigma2=1
+  output$altdistribution <- renderPlot({
+    #generate a normal distribution plot
+    ggplot(data.frame(x = c(0, 10)), aes(x = x)) +
+      stat_function(fun = dnorm, args=c(input$effectSize, 1/input$sampleSize), n=1000) 
+  })
+  
 
     
 }
