23b52ee94c15515d20b89cae5d943d1e5749cb91
Leslie <lgermain@neptuneinc.org>
2023-10-31 11:33:35 -0600

    updates to app to include sample size calculation and exercise

--- app.R
+++ app.R
@@ -14,56 +14,70 @@ ui <- fluidPage(
         sidebarPanel(
           
           
-          # solve for
-          radioButtons(inputId = "solveFor",label = "Solve for?",
-                       choices = c("Power","Significance Level","Sample Size","Effect Size"),
-                       selected = "Sample Size"),
+          # Travis I'm removing this because I'd rather keep it more simple
+          # radioButtons(inputId = "solveFor",label = "Solve for?",
+          #              choices = c("Power","Significance Level","Sample Size","Effect Size"),
+          #              selected = "Power"),
+          
+         shiny::helpText("Use this app to explore the power of a one-tailed test for a difference in means between 
+                          a test population and a reference population. Select various combinations of
+                          Type I error rate (alpha), effect size (delta - the true difference in population means),
+                          and sample size (n). Variance is assumed to be known and equal between the two populations."),
+
+         radioButtons(inputId = "sigma2",
+                      label = "Variance",
+                      choices = c(5, 10, 15, 20, 25),
+                      selected = 15, inline=TRUE),
          
-          #power
-          conditionalPanel(condition = "input.solveFor != 'Power'",
-                           sliderInput(inputId = "power",
-                                       label = "Power",
-                                       min = 0,
-                                       max = 1,
-                                       value = .8)),
-          #significance level
-          conditionalPanel(condition = "input.solveFor != 'Significance Level'",
-                           sliderInput(inputId = "alpha",
-                                       label = "Significance Level",
-                                       min = 0,
-                                       max = 1,
-                                       value = .05)),
+          # power 
+          # conditionalPanel(condition = "input.solveFor != 'Power'",
+          #  sliderInput(inputId = "power",
+          #              label = "Power",
+          #              min = 0,
+          #              max = 1,
+          #              value = .8)),
           
-          #sample size
+          # significance level
+          sliderInput(inputId = "alpha",
+                       label = "Significance Level",
+                       min = 0,
+                       max = 1,
+                       value = .05),
           
-          conditionalPanel(condition = "input.solveFor != 'Sample Size'",
-                           sliderInput(inputId = "sampleSize",
-                                       label = "Sample Size",
-                                       min = 1,
-                                       max =40,
-                                       value = 20)),
+          # sample size
+          sliderInput(inputId = "sampleSize",
+                       label = "Sample Size",
+                       min = 1,
+                       max =40,
+                       value = 20),
           
           # effect size
-          
-          conditionalPanel(condition = "input.solveFor != 'Effect Size'",
-                           sliderInput(inputId = "effectSize",
-                                       label = "Effect Size",
-                                       min = .5,
-                                       max =10,
-                                       value = 3)),
+          sliderInput(inputId = "effectSize",
+                       label = "Effect Size",
+                       min = .5,
+                       max = 4,
+                       value = 3),
       
         ),# end sidebar
 
         # Show a plot of the generated distribution
         mainPanel(
           fluidRow(
-            column(6,
-                   plotOutput("nulldistribution")
-            ),
-            column(6,
-                   plotOutput("altdistribution")
+            column(12,
+                   plotOutput("distributionPlot")
             )
-          ) # end fluidRow
+          ), # end fluidRow
+          fluidRow(
+            #Bonus Question
+            shiny::helpText("Bonus: Calculate the sample size if the effect\n
+            size is 10, the variance for both populations is 5, and the desired\n
+            power is 0.8"),
+            actionButton("go", label = "Calculate Sample Size"),
+            br(),
+            textOutput("printN"),
+            actionButton("reveal", label = "Reveal Exercise"),
+            textOutput("printExercise")
+          )
         )# end main
     )# end layout
 )# end ui
@@ -73,25 +87,47 @@ server <- function(input, output,session) {
 
   # critical value for the selected alpha
   zcrit <- reactive({
-    qnorm(1-input$alpha/2, 0, 1/input$sampleSize)
+    qnorm(1-input$alpha, 0, as.integer(input$sigma2)/input$sampleSize)
   }) # assuming two-tailed
   
-  # plot the distribution of the mean under the null, assuming mu=0 and sigma2=1
-  output$nulldistribution <- renderPlot({
-    #generate a normal distribution plot
-    ggplot(data.frame(x = c(-1, 1)), aes(x = x)) +
-      stat_function(fun = dnorm, args=c(0, 1/input$sampleSize), n=1000) +
-      geom_vline(aes(xintercept=zcrit()))
+  # min and max of the plot, reactive to selection
+  xlims <- reactive({
+    var_mean <- as.integer(input$sigma2)/input$sampleSize
+    c(-4*var_mean, input$effectSize+4*var_mean)
   })
   
-  # plot the distribution of the mean under the alternative, assuming mu=0+effectsize and sigma2=1
-  output$altdistribution <- renderPlot({
+  # plot the distribution of the mean under the reference and test distributions
+  output$distributionPlot <- renderPlot({
     #generate a normal distribution plot
-    ggplot(data.frame(x = c(0, 10)), aes(x = x)) +
-      stat_function(fun = dnorm, args=c(input$effectSize, 1/input$sampleSize), n=1000) 
+    ggplot(data.frame(x = xlims()), aes(x = x)) +
+      stat_function(fun = dnorm, args=c(0, as.integer(input$sigma2)/input$sampleSize), n=1000) +
+      geom_vline(aes(xintercept=zcrit())) +
+      stat_function(fun = dnorm, args=c(input$effectSize, as.integer(input$sigma2)/input$sampleSize), n=1000)
   })
   
-
+  calculatedN <- eventReactive(input$go, {
+    # sample size calculation
+    power <- 0.8
+    alpha <- 0.05
+    effect_size <- 2
+    z_beta <- qnorm(1-power, 0, 1)
+    z_alpha <- qnorm(alpha, 0, 1)
+    sigma <- 5
+    # sample size - round up
+    ceiling(2*((z_alpha*sigma)^2 - (z_beta*sigma)^2) / effect_size)
+  })
+  
+  output$printN <- renderText({
+    paste("The minimum sample size is:", calculatedN())
+  })
+  
+  exerciseText <- eventReactive(input$reveal, {
+    "Design a widget that will calculate sample size for any desired power input by the user. Assume alpha=0.05, effect size=5, and equal variance = 1"
+  })
+  
+  output$printExercise <- renderText({ 
+    exerciseText()
+  })
     
 }
 
